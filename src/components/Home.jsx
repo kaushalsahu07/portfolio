dc0c3771019ecb89329bbdd93ce567dc507f28b1
kaushalsahu07 <kaushal.r.sahu@outlook.com>
2025-08-01 17:11:10 +0530

    feat: Update styles and add new components
    - Change background color in index.css from #0D1117 to #000000.
    - Adjust body width to 100% for smaller screens.
    - Update tsconfig.json to include LightRays.jsx in the compilation.
    - Add GlassSurface component for creating a glass effect with customizable properties.
    - Introduce Spotlight component for animated gradient effects.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,73 +3,80 @@ import "../index.css";
 
 // Files
 import { motion } from "framer-motion";
-import { BackgroundLines } from "@/components/ui/background-lines";
-import { HoverBorderGradient } from "./ui/hover-border-gradient";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import Spotlight from "./ui/Spotlight";
+import grid from "../assets/grid.svg";
 
 function Home() {
   return (
     <section id="home">
       <div className="h-[52rem] -z-10 xl:mt-12  text-center flex flex-col justify-center items-center">
         {/* background */}
-        <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-          {/* name */}
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5, top: 10 }}
-            animate={{ opacity: 1, scale: 1, top: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.6,
-              ease: "easeInOut",
-            }}
-            className="lg:text-3xl md:text-2xl text-2xl pb-3 relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"
-          >
-            Hello! I'm Kaushal,
-          </motion.div>
-          {/* description */}
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5, top: 100 }}
-            animate={{ opacity: 1, scale: 1, top: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="text-white lg:text-3xl md:text-3xl text-2xl font-semibold max-w-[600px]"
-          >
-            Web Developer Enthusiastic about{" "}
-            <span>
-              Creating Seamless{" "}
-              <span className="bg-gradient-to-tr bg-clip-text text-transparent from-cyan-700 via-blue-500 to-indigo-600">
-                Digital Experiences.
-              </span>
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="md:block hidden" > <Spotlight /> </div>
+        <img src={grid} alt="grid" className="absolute -z-10 w-full h-full top-0 object-cover" />
+        </div>
+        {/* name */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, top: 10 }}
+          animate={{ opacity: 1, scale: 1, top: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.6,
+            ease: "easeInOut",
+          }}
+          className="lg:text-3xl  md:text-2xl text-2xl pb-3 relative z-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"
+        >
+          Hello! I'm Kaushal,
+        </motion.div>
+        {/* description */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, top: 100 }}
+          animate={{ opacity: 1, scale: 1, top: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="text-white lg:text-3xl md:text-3xl text-2xl font-semibold max-w-[600px]"
+        >
+          Web Developer Enthusiastic about{" "}
+          <span>
+            Creating Seamless{" "}
+            <span className="bg-gradient-to-tr bg-clip-text text-transparent from-cyan-700 via-blue-500 to-indigo-600">
+              Digital Experiences.
             </span>
-          </motion.div>
-          {/* button */}
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5, top: 100 }}
-            animate={{ opacity: 1, scale: 1, top: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 1,
-              ease: "easeInOut",
-            }}
-            className="mt-7 button-home"
+          </span>
+        </motion.div>
+        {/* button */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, top: 100 }}
+          animate={{ opacity: 1, scale: 1, top: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 1,
+            ease: "easeInOut",
+          }}
+          className="mt-7 button-home"
+        >
+          <a
+            href="#projects"
+            className="border border-white/30 shadow-[inset_0_0_10px_rgba(255,255,255,0.1)] shadow-white/20 rounded-full 
+              backdrop-blur-md hover:shadow-[0_0_12px_rgba(255,255,255,0.15)] text-white w-[12rem] sm:w-[14rem] h-15 
+              flex items-center justify-center 
+              transition-all duration-300 ease-in-out
+              hover:border-white/50 hover:scale-105 hover:bg-white/5"
           >
-            <HoverBorderGradient
-              containerClassName="rounded-full"
-              as="a"
-              href="#projects"
-              className="bg-[var(--color-bg)] text-white w-[12rem] sm:w-[14rem] h-12 flex items-center justify-center space-x-2"
-            >
-              <span>
-                Show my work{" "}
-                <FontAwesomeIcon className="pl-2" icon={faPaperPlane} />{" "}
-              </span>
-            </HoverBorderGradient>
-          </motion.div>
-        </BackgroundLines>
+            <span className="flex items-center transition-transform duration-300 ease-in-out hover:transform hover:translate-x-1">
+              Show my work
+              <FontAwesomeIcon
+                className="pl-2 transition-transform duration-300 group-hover:rotate-45"
+                icon={faPaperPlane}
+              />
+            </span>
+          </a>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 import { motion, AnimatePresence } from "framer-motion";
-import { useScrollDirection } from './ui/AnimatePresence';
+import { useScrollDirection } from "./ui/AnimatePresence";
+import GlassSurface from "./ui/GlassSurface";
 
 function Navbar() {
-    const scrollDirection = useScrollDirection();
+  const scrollDirection = useScrollDirection();
 
-    return(
-        <AnimatePresence>
-            <motion.div 
-                initial={{ opacity: 0, y: -100}}
-                animate={{ 
-                    opacity: 1, 
-                    y: scrollDirection === "down" ? -100 : 0
-                }}
-                transition={{  
-                    duration: 0.3,
-                    ease: "easeInOut",
-                }}
-                className='fixed top-0 left-0 right-0 z-50'>
-                <div className='grid justify-center mt-1 md:mt-3.5 rounded-xl'>
-                    <nav className='nav flex justify-center items-center pl-3 pr-3 h-19 w-86 rounded-xl m-3.5 bg-[var(--color-second)]' role='navigation'>
-                        <ul className='flex flex-wrap gap-6 text-white'>
-                            <li><a href="#" >Home</a></li>
-                            <li><a href="#about" >About</a></li>
-                            <li><a href="#projects" >Projects</a></li>
-                            <li><a href="#contact" >Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </motion.div>
-        </AnimatePresence>
-    )
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{
+          opacity: 1,
+          y: scrollDirection === "down" ? -100 : 0,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+        }}
+        className="fixed top-0 left-0 right-0 z-50"
+      >
+        <nav
+          className="nav flex justify-center items-center pl-3 pr-3 rounded-xl mt-10 mx-1.5"
+          role="navigation"
+        >
+          <GlassSurface width={700} height={75}>
+            <ul className="flex flex-wrap gap-6 text-white">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </GlassSurface>
+        </nav>
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
-export default Navbar
+export default Navbar;
