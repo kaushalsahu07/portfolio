8b8164931d21e9ef25a5875472530dbf9c012dc7
kaushalsahu07 <kaushalsahu.me@gmail.com>
2026-02-24 12:21:27 +0530

    Remove react-snowfall dependency and related component from main application

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import Snowfall from "react-snowfall";
 import "./index.css";
 
 // Web Files
@@ -14,15 +13,6 @@ import Contact from "./components/Contact";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      <Snowfall
-        style={{
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-        }}
-        radius={[0.5, 1.5]}
-        snowflakeCount={50}
-      />
       <Navbar />
       <Home />
       <About />
